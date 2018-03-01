c9afef087b1f677f25ccb7912a34f66d8411262c
garfieldmoore <garfieldmoore@gmail.com>
2018-03-01 19:16:29 +0000

    tidied code

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
+console.log("Loading dependencies:");
 var express = require('express');
-var app = express();
-
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-var apiRouter = express.Router();
-
 var GamesController = require('./js/Controllers/GamesController');
 var RatingController = require('./js/Controllers/RatingController');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+console.log("Configuring routes:");
+var apiRouter = express.Router();
 
 var gamesController = new GamesController();
 var ratingController = new RatingController();
-
-console.log("Configuring routes:")
-
-if (gamesController == undefined){
-  console.log("Games route failed")
-}
-
-if (gamesController.get == undefined){
-  console.log("Games GET route failed")
-}
-
-if (gamesController.post == undefined){
-  console.log("Games POST route failed")
-}
-
-if (ratingController == undefined){
-  console.log("Rating route failed")
-}
 
 apiRouter.route('/games')
   .get(gamesController.get)
@@ -44,6 +27,7 @@ apiRouter.route('/rating/ecf')
   apiRouter.route('/rating/uscf')
       .get(ratingController.getUscfRating);
 
+console.log("Configuring middleware");
   //CORS middleware
   var allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
@@ -54,6 +38,7 @@ apiRouter.route('/rating/ecf')
       next();
   }
 
+console.log("Configuring API");
 app.use(allowCrossDomain);
 app.use('/api', apiRouter);
 app.get('/', (req, res) => {
@@ -61,6 +46,8 @@ app.get('/', (req, res) => {
 })
 
 var port = process.env.PORT || 3000;
+
+console.log("Starting Server on port: " + port);
 
 var server = app.listen(port, function () {
   console.log("Starting Chess API Server");
